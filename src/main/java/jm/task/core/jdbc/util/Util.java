fb76dc965b837baa8c8dd54dfe8fafcc84cac77c
shilovmaxim <shilovmaxim@gmail.com>
2023-04-24 16:46:38 +0300

    Util create db connection

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,36 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost/store?serverTimezone=Europe/Moscow&useSSL=false";
+    private final static String USER = "root";
+    private final static String PASSWORD = "used!Agency5Total7";
+
+    private static final Logger LOGGER = Logger.getLogger(Util.class.getName());
+    private static int attempt = 0;
+
+    /**
+     * Делает до трех попыток подключения к БД с промежутком 1 сек.
+     *
+     * @throws SQLException if a database access error
+     * @attempt: Количество попыток подключения к базе
+     */
+    public static Connection getConnection() throws InterruptedException {
+
+        try (Connection connection = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            LOGGER.log(Level.INFO, "Successful connection to the database");
+            return connection;
+        } catch (SQLException e) {
+            LOGGER.log(Level.SEVERE, "Database connection error");
+        }
+        Thread.sleep(1000);
+        attempt++;
+        return attempt > 3 ? null : getConnection();
+    }
+
 }
