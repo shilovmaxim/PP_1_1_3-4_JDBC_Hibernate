a6e74ea5df27c03d40388557adb1aa5f5b6a48d4
shilovmaxim <shilovmaxim@gmail.com>
2023-04-25 20:20:13 +0300

    Fixed "Получи соединение к БД один раз"

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -9,15 +9,11 @@ import java.util.logging.Logger;
 public class Util {
     private final static String URL = "jdbc:mysql://localhost/store?serverTimezone=Europe/Moscow&useSSL=false";
     private final static String USER = "root";
+    //used!Agency5Total7
     private final static String PASSWORD = "used!Agency5Total7";
 
     private static final Logger LOGGER = Logger.getLogger(Util.class.getName());
-    private static int attempt = 0;
 
-    /**
-     * Делает до трех попыток подключения к БД.
-     * @attempt: Количество попыток подключения к базе
-     */
     public static Connection getConnection() {
 
         try {
@@ -27,8 +23,7 @@ public class Util {
         } catch (SQLException e) {
             LOGGER.log(Level.SEVERE, "Database connection error", e);
         }
-        attempt++;
-        return attempt > 3 ? null : getConnection();
+        return getConnection();
     }
 
 }
