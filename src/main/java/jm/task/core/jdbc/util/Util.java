3f77ed4a67680093aa90941bffeedc85c1b6bd96
shilovmaxim <shilovmaxim@gmail.com>
2023-04-27 17:25:21 +0300

    Util update

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,8 +1,13 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -10,8 +15,22 @@ public class Util {
     private final static String URL = "jdbc:mysql://localhost/store?serverTimezone=Europe/Moscow&useSSL=false";
     private final static String USER = "root";
     private final static String PASSWORD = "used!Agency5Total7";
-
     private static final Logger LOGGER = Logger.getLogger(Util.class.getName());
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE, "Database connection error", e);
+            }
+        }
+        return sessionFactory;
+    }
 
     public static Connection getConnection() {
 
